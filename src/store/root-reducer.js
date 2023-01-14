b65736f282a58fc177fed0bdb613d0bf931baef6
Allan Sanyanga <87567534+allansanyaz@users.noreply.github.com>
2023-01-14 08:19:24 +0200

    combine reducer adjustment

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from './user/user.slice';
 import cartReducer from './cart/cart.slice';
 import categoriesReducer from './categories/categories.slice';
